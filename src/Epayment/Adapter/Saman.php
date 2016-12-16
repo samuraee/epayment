d015beffca468dc7b6fcb3821d702e49d05170af
Aboozar Ghaffari <a6oozar@gmail.com>
2016-12-16 16:56:17 +0330

    finalized saman adapter by shaparak methods

--- src/Epayment/Adapter/Saman.php
+++ src/Epayment/Adapter/Saman.php
@@ -3,44 +3,27 @@ namespace Tartan\Epayment\Adapter;
 
 use SoapClient;
 use SoapFault;
-use Tartan\Epayment\Adapter\Parsian\Exception;
+use Tartan\Epayment\Adapter\Saman\Exception;
 
 class Saman extends AdapterAbstract
 {
-//	protected $WSDL         = 'https://sep.shaparak.ir/ref-payment/ws/ReferencePayment?WSDL';
-//	protected $endPoint     = 'https://sep.shaparak.ir/CardServices/controller';
-	protected $WSDL         = 'https://sep.shaparak.ir/Payments/InitPayment.asmx?WSDL';
+	protected $WSDL         = 'https://sep.shaparak.ir/payments/referencepayment.asmx?WSDL';
+	protected $tokenWSDL    = 'https://sep.shaparak.ir/Payments/InitPayment.asmx?WSDL';
+
 	protected $endPoint     = 'https://sep.shaparak.ir/Payment.aspx';
 
 	protected $testWSDL     = 'http://banktest.ir/gateway/saman/ws?wsdl';
 	protected $testEndPoint = 'http://banktest.ir/gateway/saman/gate';
 
 	protected $reverseSupport = true;
-	protected $validateReturnsAmount = true;
-
-	public function setParameters(array $options = array())
-	{
-		parent::setParameters($options);
-
-		foreach ($this->getParameters() as $name => $value) {
-			switch ($name) {
-				case 'resnum':
-						$this->order_id = $value;
-					break;
-				case 'refnum':
-						$this->ref_id = $value;
-					break;
-			}
-		}
-	}
 
 	/**
 	 * @return array
 	 * @throws Exception
 	 */
-	protected function requestToken ()
+	protected function requestToken()
 	{
-		if ($this->getInvoice()->checkForRequestToken() == false) {
+		if($this->getInvoice()->checkForRequestToken() == false) {
 			throw new Exception('epayment::epayment.could_not_request_payment');
 		}
 
@@ -60,20 +43,24 @@ class Saman extends AdapterAbstract
 		try {
 			$soapClient = new SoapClient($this->getWSDL());
 
-			$response = $soapClient->__soapCall('PinPaymentRequest', $sendParams);
+			Log::debug('RequestToken call', $sendParams);
 
-			if (isset($response->status, $response->authority)) {
-				$this->setInvoiceReferenceId($response); // update invoice reference id
-				if ($response->status == 0) {
-					return $response->authority;
-				}
-				else {
-					throw new Exception($this->status);
+			$response = $soapClient->__soapCall('RequestToken', $sendParams);
+
+			if (!empty($response))
+			{
+				Log::info('RequestToken response', ['response' => $response]);
+
+				if (strlen($response) > 10) { // got string token
+					$this->getInvoice()->setReferenceId($response); // update invoice reference id
+				} else {
+					throw new Exception($response); // negative integer as error
 				}
 			}
 			else {
-				throw new Exception('epayment::parsian.errors.invalid_response');
+				throw new Exception('epayment::epayment.invalid_response');
 			}
+
 		} catch (SoapFault $e) {
 			throw new Exception('SoapFault: ' . $e->getMessage() . ' #' . $e->getCode(), $e->getCode());
 		}
@@ -87,6 +74,7 @@ class Saman extends AdapterAbstract
 			return $this->generateFormWithoutToken(); // default
 		}
 	}
+
 	protected function generateFormWithoutToken()
 	{
 		$this->checkRequiredParameters([
@@ -96,7 +84,7 @@ class Saman extends AdapterAbstract
 			'redirect_url'
 		]);
 
-		return view('epayment::saman-redirector', [
+		return view('epayment::saman-form', [
 			'endPoint'    => $this->getEndPoint(),
 			'amount'      => intval($this->amount),
 			'merchantId'  => $this->merchant_id,
@@ -107,7 +95,7 @@ class Saman extends AdapterAbstract
 		]);
 	}
 
-	protected function generateFormWithToken(array $options = array())
+	protected function generateFormWithToken()
 	{
 		$this->checkRequiredParameters([
 			'merchant_id',
@@ -118,7 +106,7 @@ class Saman extends AdapterAbstract
 
 		$token = $this->requestToken();
 
-		return view('epayment::saman-redirector', [
+		return view('epayment::saman-form', [
 			'endPoint'    => $this->getEndPoint(),
 			'token'       => $token,
 			'redirectUrl' => $this->redirect_url,
@@ -129,6 +117,10 @@ class Saman extends AdapterAbstract
 
 	protected function verifyTransaction()
 	{
+		if($this->getInvoice()->checkForVerify() == false) {
+			throw new Exception('epayment::epayment.could_not_verify_payment');
+		}
+
 		$this->checkRequiredParameters([
 			'State',
 			'RefNum',
@@ -138,27 +130,42 @@ class Saman extends AdapterAbstract
 		]);
 
 		if ($this->State != 'OK') {
-			throw new Exception('Error: ' . $this->getStatus());
+			throw new Exception('Error: ' . $this->State);
 		}
 
 		try {
 			$soapClient = new SoapClient($this->getWSDL());
 
-			$res = $soapClient->VerifyTransaction(
-				$this->RefNum, $this->merchant_id
-			);
-		} catch (SoapFault $e) {
-			$this->_log($e->getMessage());
-			throw new Exception('SOAP Exception: ' . $e->getMessage());
-		}
+			Log::info('VerifyTransaction call', [$this->RefNum, $this->merchant_id]);
+			$response = $soapClient->VerifyTransaction($this->RefNum, $this->merchant_id);
 
-		return (int) $res;
+			if (isset($response))
+			{
+				Log::info('VerifyTransaction response', ['response' => $response]);
+
+				if ($response == $this->getInvoice()->getAmount()) { // check by invoice amount
+					$this->getInvoice()->setVerified();
+					return true;
+				} else {
+					throw new Exception($response);
+				}
+			}
+			else {
+				throw new Exception('epayment::epayment.invalid_response');
+			}
+
+		} catch (SoapFault $e) {
+			throw new Exception('SoapFault: ' . $e->getMessage() . ' #' . $e->getCode(), $e->getCode());
+		}
 	}
 
-	protected function reverseTransaction(array $options = array())
+	protected function reverseTransaction()
 	{
-		$this->setParameters($options);
-		$this->_checkRequiredOptions([
+		if ($this->reverseSupport == false || $this->getInvoice()->checkForReverse() == false) {
+			throw new Exception('epayment::epayment.could_not_reverse_payment');
+		}
+
+		$this->checkRequiredParameters([
 			'ref_id',
 			'merchant_id',
 			'password',
@@ -168,20 +175,44 @@ class Saman extends AdapterAbstract
 		try {
 			$soapClient = new SoapClient($this->getWSDL());
 
-			$res = $soapClient->reverseTransaction(
+			Log::info('reverseTransaction call', [$this->RefNum, $this->merchant_id]);
+			$response = $soapClient->reverseTransaction(
 				$this->ref_id,
 				$this->merchant_id,
 				$this->password,
 				$this->amount
 			);
-		} catch (SoapFault $e) {
-			$this->_log($e->getMessage());
-			throw new Exception('SOAP Exception: ' . $e->getMessage());
-		}
 
-		return (int) $res;
+			if (isset($response))
+			{
+				Log::info('reverseTransaction response', ['response' => $response]);
+
+				if ($response === 1) { // check by invoice amount
+					$this->getInvoice()->setReversed();
+					return true;
+				} else {
+					throw new Exception($response);
+				}
+			}
+			else {
+				throw new Exception('epayment::epayment.invalid_response');
+			}
+
+		} catch (SoapFault $e) {
+			throw new Exception('SoapFault: ' . $e->getMessage() . ' #' . $e->getCode(), $e->getCode());
+		}
 	}
 
+	/**
+	 * @return bool
+	 */
+	public function canContinueWithCallbackParameters()
+	{
+		if ($this->State == 'OK') {
+			return true;
+		}
+		return false;
+	}
 
 	public function getGatewayReferenceId()
 	{
@@ -189,5 +220,21 @@ class Saman extends AdapterAbstract
 			'RefNum',
 		]);
 		return $this->RefNum;
+	}
+
+	protected function getWSDL ($type = null)
+	{
+		if (config('epayment.mode') == 'production') {
+			switch (strtoupper($type)) {
+				case 'TOKEN':
+					return $this->tokenWSDL;
+					break;
+				default:
+					return $this->WSDL;
+					break;
+			}
+		} else {
+			return $this->testWSDL;
+		}
 	}
 }
