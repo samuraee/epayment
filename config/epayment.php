9031fab546cd1b133763c3484de329dbe80a7bab
Aboozar Ghaffari <a6oozar@gmail.com>
2016-12-18 23:10:17 +0330

    removed extra config

--- config/epayment.php
+++ config/epayment.php
@@ -58,7 +58,6 @@ return [
 	*/
 	'parsian'  => [
 		'pin'          => env('PARSIAN_PIN', ''),
-		'callback_url' => env('PARSIAN_CALLBACK_URL', '')
 	],
 	/*
 	|--------------------------------------------------------------------------
